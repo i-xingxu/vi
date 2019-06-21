510166c37dae9af7b6026397f72a81267c400e39
xingxu <89942589@qq.com>
2019-06-21 13:59:08 +0800

    '优化截图'

--- base/webbase.py
+++ base/webbase.py
@@ -198,7 +198,7 @@ class Web():
             self.driver.set_window_size(scroll_width, scroll_height)
             self.driver.save_screenshot(picNam)
             f = open(picNam,'rb').read()
-            allure.attach('This is a picture',f,allure.attachment_type.PNG)
+            allure.attach.file('This is a picture',f,allure.attachment_type.PNG)
         except Exception as e:
             self.lg.error(e)
             self.lg.error("获取截图失败！")
